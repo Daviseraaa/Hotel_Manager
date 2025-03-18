9c3b5638d64ab7f2297f071c7eea7b08db8e2c35
Daviseraaa <trantuandoan04@gmail.com>
2025-03-18 15:05:32 +0700

    remove bycript add bycripyjs

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const userModel = require('../models/userModel.js');
 const creditModel = require('../models/creditModel.js');
 const roomModel = require('../models/roomModel.js')
